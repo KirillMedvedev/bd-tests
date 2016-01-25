0b8131db10a7fcd4303352811b2d4fd8976aea39
Kirill Medvedev <Kirill Medvedev>
2016-01-24 20:52:08 -0600

    First draft to share with team members

---
+++ library/bd.mstest.tests/given_no_system_under_test/when_running_test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bd.mstest.tests.given_no_system_under_test
+{
+    [TestClass]
+    public class when_running_test : SUT
+    {
+        protected override void Arrange()
+        {
+            callsSequence.Add("Arrange");
+        }
+
+        protected override void Act()
+        {
+            callsSequence.Add("Act");
+        }
+
+        [TestMethod]
+        public void then_arrange_works_first()
+        {
+            Assert.AreEqual("Arrange", callsSequence[0]);
+        }
+
+        [TestMethod]
+        public void then_act_works_next()
+        {
+            Assert.AreEqual("Act", callsSequence[1]);
+        }
+
+        [TestMethod]
+        public void then_assert_called_next()
+        {
+            callsSequence.Add("Assert");
+
+            Assert.AreEqual("Assert", callsSequence[2]);
+        }
+
+        List<string> callsSequence = new List<string>();
+    }
+}
---
+++ library/bd.mstest/SUT.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bd.mstest
+{
+    public class SUT
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Arrange();
+            Act();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Clean();
+        }
+
+        protected virtual void Arrange() { }
+        protected virtual void Act() { }
+        protected virtual void Clean() { }
+    }
+}
